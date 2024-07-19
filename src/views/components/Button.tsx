4cb0f7f4d15ead82fa05487b1303b1df94daaaf8
mnava1418 <martin@mnavapena.com>
2024-07-18 22:45:57 -0600

    UPDATE: handle multiple button types

--- src/views/components/Button.tsx
+++ src/views/components/Button.tsx
@@ -5,23 +5,38 @@ import { globalStyles } from '../../styles/mainStyles'
 type ButtonProps = {
     label: string,
     onPress: () => void,
+    type: 'primary' | 'secondary'
     style?: {}
 }
 
-const Button = ({label, onPress, style = {}}: ButtonProps) => {
-  return (
-    <View style={{...style, ...globalStyles.btnContainer}}>
-        <Pressable 
-            style={({pressed}) => ({
-                ...globalStyles.btn,
-                opacity: pressed ? 0.8 : 1.0
-            })} 
-            onPress={() => {onPress()}}
-        >
-            <Text style={globalStyles.btnTxt}>{label}</Text>
-        </Pressable>
-    </View>
-  )
+const Button = ({label, type, onPress, style = {}}: ButtonProps) => {
+    let btnStyles = globalStyles.btn
+    let btnTxtStyles = globalStyles.btnTxt
+
+    switch (type) {
+        case 'primary':
+            btnStyles = {...btnStyles, ...globalStyles.btnPrimary}
+            btnTxtStyles = {...btnTxtStyles, ...globalStyles.btnTxtPrimary}
+            break;
+        case 'secondary':
+            btnStyles = {...btnStyles, ...globalStyles.btnSecondary}
+        default:
+            break;
+    }
+
+    return (
+        <View style={{...style, ...globalStyles.btnContainer}}>
+            <Pressable 
+                style={({pressed}) => ({
+                    ...btnStyles,
+                    opacity: pressed && type === 'primary' ? 0.8 : 1.0
+                })} 
+                onPress={() => {onPress()}}
+            >
+                <Text style={btnTxtStyles}>{label}</Text>
+            </Pressable>
+        </View>
+    )
 }
 
 export default Button
